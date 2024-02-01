51bb508ed24f9212ae445ccc1aa7e2534ed6fa29
jgazegers <jga.zegers@student.avans.nl>
2024-02-01 12:33:43 +0100

    Add mongoDB database and user routes for week 1

--- week1/routes/users.js
+++ week1/routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const {db} = require('../services/database');
+
+router.get('/', async function(req, res, next) {
+  let users = await db.collection('users').find().toArray();
+  res.json(users);
 });
+
+router.post('/', function(req, res, next) {
+  db.collection('users').insertOne(req.body)
+      .then((user) => res.status(201).json({ "id": user.insertedId}))
+      .catch(err => res.status(500).json(err));
+})
 
 module.exports = router;
